069a381a18e952af96ffac9784180e5892a0c124
alanfzf <alandavidg13@gmail.com>
2024-08-15 09:40:34 -0600

    fix(bat)!: use apt instead of own script

--- src/bat/install.sh
+++ src/bat/install.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 set -e
 
-BAT_VERSION=$(curl -s "https://api.github.com/repos/sharkdp/bat/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+# BAT_VERSION=$(curl -s "https://api.github.com/repos/sharkdp/bat/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+# ARCH="$(uname -m)"
 
-ARCH="$(uname -m)"
+# if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "aarch64" ]; then
+#   echo "Unsupported architecture"
+#   exit 1
+# fi
 
-if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "aarch64" ]; then
-  echo "Unsupported architecture"
-  exit 1
-fi
+# BASE_URL="https://github.com/sharkdp/bat/releases/latest/download/bat-v${BAT_VERSION}-${ARCH}-unknown-linux-gnu.tar.gz"
 
-BASE_URL="https://github.com/sharkdp/bat/releases/latest/download/bat-v${BAT_VERSION}-${ARCH}-unknown-linux-gnu.tar.gz"
+# curl -sL ${BASE_URL} | tar --strip-components=1 -xz
+# install bat /usr/local/bin
 
-curl -sL ${BASE_URL} | tar --strip-components=1 -xz
-install bat /usr/local/bin
+apt-get update && apt-get -y install bat
+ln -s /usr/bin/batcat /usr/local/bin/bat
